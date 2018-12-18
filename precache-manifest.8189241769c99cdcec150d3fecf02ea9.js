3754122099ed55cd9bdf6e3ed34d8e5b8cdade2e
Jon Song <jonsong246@gmail.com>
2018-12-18 02:57:44 -0800

    commit

---
+++ precache-manifest.8189241769c99cdcec150d3fecf02ea9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "59843ae1c2ed89599fd0",
+    "url": "/ClickyGame/static/js/runtime~main.59843ae1.js"
+  },
+  {
+    "revision": "6c1a605bcfe4b52de19a",
+    "url": "/ClickyGame/static/js/main.6c1a605b.chunk.js"
+  },
+  {
+    "revision": "3ad5f8a614c5e526e94e",
+    "url": "/ClickyGame/static/js/1.3ad5f8a6.chunk.js"
+  },
+  {
+    "revision": "6c1a605bcfe4b52de19a",
+    "url": "/ClickyGame/static/css/main.e5d637b5.chunk.css"
+  },
+  {
+    "revision": "2ad020b6c7b5334478200ad52f98dc4b",
+    "url": "/ClickyGame/index.html"
+  }
+];
